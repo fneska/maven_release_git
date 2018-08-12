189e03aa1580ef7cc92a4718d9497f066e977fe5
Fekete Agnes <Fekete.Agnes@prolan.hu>
2018-08-12 13:39:10 +0200

    c

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
-call mvn release:prepare -DallowTimestampedSnapshots=true -DautoVersionSubmodules=true -DpushChanges=true -DtagBase=scm:git:https://github.com/fneska/maven_release_git/tag -Dusername=fneska -Dpassword=jamiNa73 -DupdateDependencies=true -DreleaseVersion=1.9.3 -DdevelopmentVersion=1.9.4-SNAPSHOT
+call mvn release:prepare -DallowTimestampedSnapshots=true -DautoVersionSubmodules=true -DpushChanges=true -DtagBase=scm:git:https://github.com/fneska/maven_release_git/tag -Dusername=fneska -Dpassword=jamiNa73 -DupdateDependencies=true -DreleaseVersion=1.9.45 -DdevelopmentVersion=1.9.46-SNAPSHOT
 
 call mvn release:perform -Dusername=fneska -Dpassword=jamiNa73
